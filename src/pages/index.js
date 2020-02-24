67037d4a11f66f990d5cacfc03fbe64975a337bf
Rami Mustaklem <ramimustaklem@gmail.com>
2020-02-24 22:33:00 +0200

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import styled from "styled-components"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
+
+export default ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <div>
+      <h1>Rami's Thoughts</h1>
+      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </BlogTitle>
+          </BlogLink>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export const query = graphql`
+  query MyQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          id
+          html
+          frontmatter {
+            description
+            title
+            date
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
